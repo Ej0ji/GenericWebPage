7490d1978134881037cdf72df2e68bf595b87a1d
Eric Joji <ericjoji303@gmail.com>
2026-04-05 15:40:09 -0300

    Creating templates folder
    Creating templates folder with the html template for the homepage;
    
    Importing the render_template in views to it has access to the templates.

--- views.py
+++ views.py
@@ -1,9 +1,15 @@
 # Importing all dependences from the main
 from main import app
+from flask import render_template
 
 # -------------- Defining routes --------------
 
 # Homepage route
 @app.route("/") # @ means a decorator. It adds an extra configuration to a function
 def homepage():
+    return render_template("homepage.html")
+
+# Infopage route
+@app.route("/infos")
+def infopage():
     return "Olá, mundo!"
