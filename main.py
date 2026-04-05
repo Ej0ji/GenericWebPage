7659150a43b75fa4bd708f8f829941bc64bd04ca
Eric Joji <ericjoji303@gmail.com>
2026-04-05 15:21:27 -0300

    Update main.py
    Creating the first route to the homepage from the site

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
+# Initializing the project
 from flask import Flask
-print("Olá, Mundo!")
+
+app = Flask(__name__)
+
+# -------------- Defining routes --------------
+
+# Homepage route
+@app.route("/") # @ means a decorator. It adds an extra configuration to a function
+def homepage():
+    return "Olá, mundo!"
+
+# This "if" were created to guarantee that the app runs just when the main.py is directly executed 
+if __name__ == "__main__": 
+    app.run()
+
