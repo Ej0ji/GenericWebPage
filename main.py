6bd8e9eca9d953f96afa53a12ae182e9886468a1
Eric Joji <ericjoji303@gmail.com>
2026-04-05 16:17:14 -0300

    Updating main.py & views.py
    Activating the debug mode in the main;
    
    Editing a comment in views.

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ from views import *
 
 # This "if" were created to guarantee that the app runs just when the main.py is directly executed 
 if __name__ == "__main__": 
-    app.run()
+    app.run(debug=True) #debug=True is used to not need to run main everytime that is added a new change 
 
--- views.py
+++ views.py
@@ -5,7 +5,7 @@ from flask import render_template
 # -------------- Defining routes --------------
 
 # Homepage route
-@app.route("/") # @ means a decorator. It adds an extra configuration to a function
+@app.route("/") # @ means a decorator. It adds an extra configuration to a function. In this case, the attribute represents the path after the domain
 def homepage():
     return render_template("homepage.html")
 
